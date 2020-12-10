4801af708129297782754d311212e3b38829a8ba
Aakash_Deep <51085133+bryophyllum1@users.noreply.github.com>
2020-12-10 08:39:57 +0530

    Taking input and printing to the console.
    Used readlineSync package to read input.

---
+++ Week1/ex_02.js
@@ -0,0 +1,6 @@
+// To install readlineSync package
+// npm i readline-sync
+
+var readlineSync = require('readline-sync');
+var userName = readlineSync("Hey man, what is your name ?");
+console.log("your name is "+ userName);
