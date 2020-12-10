ce8b0635d378ed82a8f7e3f78670f2458c026228
Aakash_Deep <51085133+bryophyllum1@users.noreply.github.com>
2020-12-10 23:10:59 +0530

    object

---
+++ Week1/ex_12.js
@@ -0,0 +1,18 @@
+var superman={
+  speciality:"lasers",
+  intelligence:"89",
+  popularity:"70",
+}
+var batman={
+  speciality :"technology",
+  intelligence :"100",
+  popularity :"100",
+}
+var car={
+  manufacturer :"Lamborghini",
+  model :"aventador",
+  engine :"v8",
+  type :"petrol",
+
+}
+console.log(car.manufacturer)
